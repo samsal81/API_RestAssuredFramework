f9591000945d229ea6acb5d87ca31037a0a16f13
Osama Alsaleh <samsal81@outlook.com>
2021-02-22 15:36:45 -0500

    some fixes

--- src/test/java/tests/getOneItem.java
+++ src/test/java/tests/getOneItem.java
@@ -26,17 +26,17 @@ public class getOneItem {
 		
 		ResponseBody respbody = response.body();
 		String respoBody = respbody.asString();
-		System.out.println(respoBody);
+		//System.out.println(respoBody);
 		
 		JsonPath jp = new JsonPath(respoBody); 
-		//int id = js.getInt("id");
+		int id = jp.getInt("data.id");
 		String email = jp.get("data.email");
 		String FN = jp.get("data.first_name");
 		String LN = jp.get("data.last_name");
 		String avatar = jp.get("data.avatar");
 		
-		//System.out.println("id: "+id);
-		System.out.println("email: " +email);
+		System.out.println("Id: "+id);
+		System.out.println("Email: " +email);
 		System.out.println("First Name: " +FN);
 		System.out.println("Last Name: " +LN);
 		System.out.println("Avatar: " +avatar);
