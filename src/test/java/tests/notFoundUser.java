4174a86687eb5674f4a761bf0a1b7bb8de6eec12
Osama Alsaleh <samsal81@outlook.com>
2021-02-22 15:44:35 -0500

    added user not found test

---
+++ src/test/java/tests/notFoundUser.java
@@ -0,0 +1,30 @@
+package tests;
+
+import static io.restassured.RestAssured.given;
+
+import org.testng.annotations.Test;
+
+import io.restassured.response.Response;
+
+public class notFoundUser {
+
+	@Test
+	public void UnfoundUser() {
+		Response response = 
+				given()
+				.baseUri("https://reqres.in/api/")
+				.header("Content-Type", "application/json; charset=utf-8")
+				.queryParam("id", "100")
+				.when()
+				.log().all()
+				.get("users")
+				.then()
+				.log().all()
+				.statusCode(404)
+				.header("Content-Type", "application/json; charset=utf-8")
+				.extract().response();
+		
+		String resp = response.asString();
+		System.out.println(resp);
+	}
+}
